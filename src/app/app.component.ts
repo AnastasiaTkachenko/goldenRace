85f375ae0f4ab3e1ab428bb192659ac232273630
anastasia <anastasitkachenko@gmail.com>
2022-01-24 20:32:52 +0100

    creating a structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'goldenRace';
+
+ /* d
+export class AppComponent implements OnInit {
+  balls:any=[];
+  results:any;
+  chooseBalls: number[]=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  preSelectBalls:number[]=[];
+  selectedNumber:number=0; 
+
+ ngOnInit(): void {
+    this.selectedNumber?.subscribe((newNumber:number)=>{
+      this.preSelectBalls.push(newNumber);
+    })
+   
+  } 
+  selectNumber(item:number){
+    this.selectedNumber=item; 
+
+  
+  }
+
+
+randomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+  
 }
+
+
+  clearSelector(){
+this.selectedNumber=0; 
+    
+  }
+  
+
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
